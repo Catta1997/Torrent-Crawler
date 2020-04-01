ceb9960d6e89e6c3a3a7ffed785ac0d2259939b5
Catta1997 <34333654+Catta1997@users.noreply.github.com>
2020-04-01 14:41:57 +0200

    Magnet Link fix + autoadd feature

--- torrent.py
+++ torrent.py
@@ -1,5 +1,5 @@
 # importing the requests library 
-import requests
+import requests,os
 from bs4 import BeautifulSoup
 
 
@@ -11,7 +11,37 @@ result = ['']
 URL = "https://www.1377x.to/category-search/" + name + "/Movies/1/"
 PARAMS = {}
 
+add_torrent_command = 'addtorr'
+autoadd = True
 
+def bubbleSort(arr,arr2,arr3,arr4):
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n):
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                arr2[j], arr2[j+1] = arr2[j+1], arr2[j]
+                arr3[j], arr3[j+1] = arr3[j+1], arr3[j]
+                arr4[j], arr4[j+1] = arr4[j+1], arr4[j]
+
+
+def bubbleSort2(arr,arr2):
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n):
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                arr2[j], arr2[j+1] = arr2[j+1], arr2[j]
 def split_and_keep(s, sep):
     if not s: return ['']
     p=chr(ord(max(s))+1)
@@ -35,10 +65,19 @@ if len(title_box)== 0:
     exit(1)
 print("%d" %len(title_box) + " torrents founded for \"" + name_input +"\"")
 print("")
+
+GB_ord_ext = list()
+GB_ord = list()
+MB_ord_ext = list()
+MB_ord = list()
+name_torrent = list()
+
 for dim in parsed_html.findAll('td', attrs={'class':'coll-4'}):
     GB.append(dim.text)
     x+=1
+
 x = 0
+
 for parsed in parsed_html.findAll('td', attrs={'class':'coll-1 name'}):
     print('---------')
     print('Torrent %d' %(x+1) + ":")
@@ -47,19 +86,55 @@ for parsed in parsed_html.findAll('td', attrs={'class':'coll-1 name'}):
         if(string_res != ""):
             line = GB[x]
             dim = line
-
-#        echo -e "\t\x1b[32mAttempt $i: Success\x1b[0m"
- #       Success=$((Success + 1))
-#    else
-#        echo -e "\t\x1b[31mAttempt $i: Fail\x1b[0m"#
-#
-
-
             print("\x1b[36m" + string_res + "\x1b[0m")
             print("\x1b[32mDIM: " + split_and_keep(dim, 'B')[0]+ "\x1b[0m" )
+            print(a['href'])
         if('torrent' in a['href']):
             result.append('https://www.1377x.to' + a['href'])
     x += 1
+print("")
+print("ordino")
+print("")
+
+x = 0
+for parsed in parsed_html.findAll('td', attrs={'class':'coll-1 name'}):
+    for a in parsed.find_all('a', href=True):
+        string_res = (a.text)
+        if(string_res != ""):
+            line = GB[x]
+            dim = line
+            dims = split_and_keep(dim, 'B')[0]
+            name_torrent.append(string_res)
+            if "GB" in dims:
+                dims = dims[: -3]
+                GB_ord.append(dims)
+                GB_ord_ext.append(split_and_keep(dim, 'B')[0])
+            if "MB" in dims:
+                dims = dims[: -3]
+                MB_ord.append(dims)
+                MB_ord_ext.append(split_and_keep(dim, 'B')[0])
+            bubbleSort(GB_ord,name_torrent,result,GB_ord_ext)
+            bubbleSort2(MB_ord,MB_ord_ext)
+    x += 1
+
+x = 0
+torrent = 1
+for elem in MB_ord:
+    print('---------')
+    print('Torrent %d' %(torrent) + ":")
+    print("\x1b[36m" + name_torrent[x] + "\x1b[0m")
+    print("\x1b[32mDIM: " + MB_ord_ext[x]+ "\x1b[0m" )
+    torrent+=1
+    x+=1
+x = 0
+for elem in GB_ord:
+    print('---------')
+    print('Torrent %d' %(torrent) + ":")
+    print("\x1b[36m" + name_torrent[x] + "\x1b[0m")
+    print("\x1b[32mDIM: " + GB_ord_ext[x]+ "\x1b[0m" )
+    torrent+=1
+    x+=1
+
 print('---------')
 print("")
 found = 0
@@ -68,8 +143,31 @@ while(found == 0):
     print("")
     number = input('Choose torrent:')
     if(number < len(result)):
+
+        URL = result[number]
+        r = requests.get(url = URL, params = PARAMS) 
+        # extracting data in json format 
+        pastebin_url = r.text 
+        #print("The pastebin URL is:%s"%pastebin_url) 
+        html = pastebin_url
+        parsed_html = BeautifulSoup(html,"html.parser")
+        magnet_link = ''
+        #title_box = parsed_html.findAll('li')
+        for parsed in parsed_html.findAll('li'):
+            for a in parsed.find_all('a', href=True):
+                if('magnet' in a['href']):
+                    magnet_link = a['href']
+
+
         print("")
-        print("Magnet: \x1b[31;1m" + result[number]+ "\x1b[0m")
+        print("Magnet: \x1b[31;1m" + magnet_link + "\x1b[0m")
+        print("")
+        print("")
+        if(autoadd):
+            if os.system(add_torrent_command + ' ' + magnet_link):
+                print('\x1b[32mSuccess\x1b[0m')
+            else:
+                print("\x1b[31;1mError\x1b[0m")
         found = 1
         print("")
     else : 
