63665e8831692f3e4c6b8017c7eaecce2797c810
Catta1997 <edoardo.cattarin@gmail.com>
2022-03-22 15:11:20 +0100

    fix

--- OTHER/torrent.py
+++ OTHER/torrent.py
@@ -1,140 +1,116 @@
-# importing the requests library 
-import requests,os,subprocess
+# importing the requests library
+import requests
+import os
 import sys
 from bs4 import BeautifulSoup
-
-#config
+import json
+# config
 torrent_pages = 3
 add_torrent_command = 'transmission-remote -n transmission:transmission -a'
 autoadd = True
-#end config
+# end config
 
 resulst = list()
 GB = list()
 result = ['']
 torrent = 1
-GB_ord_ext = list()
-GB_ord = list()
-MB_ord_ext = list()
-MB_ord = list()
 name_torrent = list()
-def bubbleSort(arr,arr2,arr3,arr4):
-    n = len(arr)
-    # Traverse through all array elements
-    for i in range(n):
-        # Last i elements are already in place
-        for j in range(0, n-i-1):
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            arr[j] = arr[j].replace(',', '')
-            arr[j+1] = arr[j+1].replace(',', '')
-            z = float(arr[j])
-            x = float(arr[j+1])
-            if z > x :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                arr2[j], arr2[j+1] = arr2[j+1], arr2[j]
-                arr3[j], arr3[j+1] = arr3[j+1], arr3[j]
-                arr4[j], arr4[j+1] = arr4[j+1], arr4[j]
-def bubbleSort2(arr,arr2):
-    n = len(arr)
-    # Traverse through all array elements
-    for i in range(n):
-        # Last i elements are already in place
-        for j in range(0, n-i-1):
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            arr[j] = arr[j].replace(',', '')
-            arr[j+1] = arr[j+1].replace(',', '')
-            z = float(arr[j])
-            x = float(arr[j+1])
-            if z > x :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                arr2[j], arr2[j+1] = arr2[j+1], arr2[j]
+json_torrent = '''
+{
+    "Torrent": [
+        ]
+}
+'''
+
+
 def split_and_keep(s, sep):
-    if not s: return ['']
-    p=chr(ord(max(s))+1)
+    if not s:
+        return ['']
+    p = chr(ord(max(s))+1)
     return s.replace(sep, sep+p).split(p)
-def search1337x(r,name_s):
+
+
+def search1337x(r, name_s):
     global torrent
-    global GB_ord_ext
-    global GB_ord
-    global MB_ord_ext
-    global MB_ord
     global name_torrent
-    # extracting data in json format 
-    pastebin_url = r.text 
-    #print("The pastebin URL is:%s"%pastebin_url) 
+    global json_torrent
+    # extracting data in json format
+    pastebin_url = r.text
+    #print("The pastebin URL is:%s"%pastebin_url)
     html = pastebin_url
-    parsed_html = BeautifulSoup(html,"html.parser")
-    title_box = parsed_html.findAll('td', attrs={'class':'coll-1 name'})
+    parsed_html = BeautifulSoup(html, "html.parser")
+    title_box = parsed_html.findAll('td', attrs={'class': 'coll-1 name'})
     #page_link = parsed_html.findAll('div', attrs={'class':'pagination'})
     x = 0
-    if len(title_box)== 0:
+    if len(title_box) == 0:
         print("No torrent founded for \"" + name_s+"\"")
         print("")
         exit(1)
-    #Get Torrent Size
-    for dim in parsed_html.findAll('td', attrs={'class':'coll-4'}):
+    # Get Torrent Size
+    for dim in parsed_html.findAll('td', attrs={'class': 'coll-4'}):
         GB.append(dim.text)
-        x+=1
-    x = 0
-    #Get torrent name
-    for parsed in title_box:
-        for a in parsed.find_all('a', href=True):
-            string_res = (a.text)
-            if(string_res != ""):
-                line = GB[x]
-                dim = line
-            if('torrent' in a['href']):
-                result.append('https://www.1377x.to' + a['href'])
         x += 1
     x = 0
-    #Dim
+    # Get torrent info
     for parsed in title_box:
         for a in parsed.find_all('a', href=True):
             string_res = (a.text)
-            if(string_res != ""):
+            if(len(string_res) > 1):
                 line = GB[x]
                 dim = line
-                dims = split_and_keep(dim, 'B')[0]
                 name_torrent.append(string_res)
-                if "GB" in dims:
-                    dims = dims[: -3]
-                    GB_ord.append(dims)
-                    GB_ord_ext.append(split_and_keep(dim, 'B')[0])
-                if "MB" in dims:
-                    dims = dims[: -3]
-                    MB_ord.append(dims)
-                    MB_ord_ext.append(split_and_keep(dim, 'B')[0])
+                temp = {
+                    'name': string_res,
+                    # split_and_keep(dim, ' ')[0],
+                    'size': float(dim.split(" ")[0]),
+                    'type': split_and_keep(dim, ' ')[-1],
+                    'link': 'https://www.1377x.to' + a['href']
+                }
+                data = json.loads(json_torrent)
+                t_list = data['Torrent']
+                # print(t_list)
+                t_list.append(temp)
+                json_torrent = json.dumps(data, indent=4)
+                sorted_obj = dict(data)
+                sorted_obj['Torrent'] = sorted(
+                    data['Torrent'], key=lambda x: x['size'], reverse=True)
+                json_torrent = json.dumps(sorted_obj, indent=4)
         x += 1
+    x = 0
+    item_dict = json.loads(json_torrent)
+    print(len(item_dict['Torrent']))
+
+
 def search1377x_request(name_s):
     global torrent_pages
-    # sending get request and saving the response as response object 
+    # sending get request and saving the response as response object
     max = torrent_pages
-    for c in range(1,max + 1):
-        URL = "https://www.1377x.to/search/" + name_s + "/" + "%d"%c + "/"
-        r = requests.get(url = URL, params = {}) 
-        search1337x(r,name_s)
+    for c in range(1, max + 1):
+        URL = "https://www.1377x.to/search/" + name_s + "/" + "%d" % c + "/"
+        r = requests.get(url=URL, params={})
+        search1337x(r, name_s)
+
+
 def sort():
     global torrent
-    global GB_ord_ext
-    global GB_ord
-    global MB_ord_ext
-    global MB_ord
+
     global name_torrent
-    bubbleSort(GB_ord,name_torrent,result,GB_ord_ext)
-    bubbleSort2(MB_ord,MB_ord_ext)
+    global json_torrent
+   # bubbleSort(GB_ord,name_torrent,result,GB_ord_ext)
+    # bubbleSort2(MB_ord,MB_ord_ext)
     x = 0
-    dim_tot = MB_ord_ext + GB_ord_ext
-    for elem in dim_tot:
+
+    data = json.loads(json_torrent)
+    for elem in data['Torrent']:
         print('---------')
-        print('Torrent %d' %(torrent) + ":")
-        print("\x1b[36mTITLE: " + name_torrent[x] + "\x1b[0m")
-        print("\x1b[32mDIM: " + dim_tot[x]+ "\x1b[0m" )
-        torrent+=1
-        x+=1
+        print('Torrent %d' % (torrent) + ":")
+        print("\x1b[36mTITLE: " + elem['name'] + "\x1b[0m")
+        print("\x1b[32mDIM: " + str(elem['size']) +
+              " " + elem['type'] + "\x1b[0m")
+        torrent += 1
+        x += 1
+
+
 def select():
     print('---------')
     print("")
@@ -142,17 +118,19 @@ def select():
     while(found == 0):
         print("")
         print("")
-        number = input('Choose torrent:')
-        if(number < len(result)):
-            URL = result[number]
-            r = requests.get(url = URL, params = {}) 
+        item_dict = json.loads(json_torrent)
+        number = int(input('Choose torrent:'))
+        number -= 1  # indice di un array
+        if(number < len(item_dict['Torrent'])):
+            item_dict = json.loads(json_torrent)['Torrent'][number]
+            URL = item_dict['link']
+            r = requests.get(url=URL, params={})
             # extracting data in json format
             pastebin_url = r.text
             html = pastebin_url
-            parsed_html = BeautifulSoup(html,"html.parser")
-            #print("The pastebin URL is:%s"%pastebin_url) 
+            parsed_html = BeautifulSoup(html, "html.parser")
+            #print("The pastebin URL is:%s"%pastebin_url)
             magnet_link = ''
-            dim_tot = MB_ord_ext + GB_ord_ext
             #title_box = parsed_html.findAll('li')
             for parsed in parsed_html.findAll('li'):
                 for a in parsed.find_all('a', href=True):
@@ -160,14 +138,16 @@ def select():
                         magnet_link = a['href']
             print("")
             found = 1
-            print("\x1b[36mTITLE: " + name_torrent[number] + "\x1b[0m")
-            print("\x1b[32mDIM: " + dim_tot[number]+ "\x1b[0m" )
-            conf = raw_input("y to confirm, n to repeat: ")
+            print("\x1b[36mTITLE: " + item_dict['name'] + "\x1b[0m")
+            print("\x1b[32mDIM: " + str(item_dict['size']) +
+                  " " + item_dict['type'] + "\x1b[0m")
+            conf = input("y to confirm, n to repeat: ")
             print("")
             if(conf == 'n' or conf == 'N'):
                 found = 0
             elif(autoadd and (conf == 'y' or conf == 'Y')):
-                command = add_torrent_command + ' \'' + magnet_link + '\' >&- 2> add_torrent_output.txt'
+                command = add_torrent_command + ' \'' + \
+                    magnet_link + '\' >&- 2> add_torrent_output.txt'
                 os.system(command)
                 with open('add_torrent_output.txt', 'r') as f:
                     if 'command not found' not in f.read():
@@ -176,24 +156,28 @@ def select():
                         print("\x1b[31;1mError, command not found\x1b[0m")
                         print("")
                         print('Magnet:\x1b[31;1m ' + magnet_link + "\x1b[0m")
-            else :
+            else:
                 print("Magnet: \x1b[31;1m" + magnet_link + "\x1b[0m")
                 print("")
                 print("")
             print("")
-        else :
+        else:
             print("")
             print("\x1b[31;1mOut of range\x1b[0m")
+
+
 def main():
-    if len (sys.argv) == 1 :
-        name_input = raw_input('Nome Film da cercare:').strip()
-    else :
+    if len(sys.argv) == 1:
+        name_input = input('Nome Film da cercare:').strip()
+    else:
         name_input = sys.argv[1]
         for x in sys.argv[2:]:
             name_input += ' ' + x
-    name = str(name_input).replace(' ' , '%20')
+    name = str(name_input).replace(' ', '%20')
     search1377x_request(name)
     sort()
     select()
+
+
 if __name__ == "__main__":
     main()
