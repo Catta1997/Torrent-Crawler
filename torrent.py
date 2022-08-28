795c916253daf9132f6b5253ff395b180e25f9a8
Sinon <34333654+Catta1997@users.noreply.github.com>
2022-08-28 13:31:43 +0200

    code
    no new feature

--- torrent.py
+++ torrent.py
@@ -9,6 +9,17 @@ from bs4 import BeautifulSoup
 
 class TorrentDownloader():
     '''Torrent magnet link'''
+    #text format
+    bold_text = "\033[1m"
+    underscore = "\x1b[4m"
+    reset_clr = "\x1b[0m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    magenta = "\x1b[35m"
+    cyan = "\x1b[36m"
+    white = "\x1b[37m"
     # config
     torrent_pages = 3
     QNAP = 0
@@ -81,7 +92,7 @@ class TorrentDownloader():
             if elem == 1:
                 parsed_html = BeautifulSoup(req.text, "html.parser")
                 if len(parsed_html.findAll('tr')) == 1:
-                    print(f"\x1b[31;1mNo torrent founded for \"{name_s}\"\x1b[0m")
+                    print(f"{self.red}No torrent founded for \"{name_s}\"{self.reset_clr}")
                     print("")
                     sys.exit(0)
             self.search1337x(req)
@@ -91,42 +102,39 @@ class TorrentDownloader():
         torrent = 1
         data = json.loads(self.json_torrent)
         for elem in data['Torrent']:
-            print('\033[4m')
-            print(' '*120)
-            print('\x1b[0m')
-            print(f" \033[1mTorrent {torrent} :\x1b[0m")
+            # write _____________
+            print(f'{self.underscore}' + ' '*120 + f'{self.reset_clr}\n')
+            print(f" {self.bold_text}Torrent {torrent} :{self.reset_clr}")
             self.print_elem(elem)
             torrent += 1
 
     def print_elem(self,elem):
         '''Print torrent element'''
         title_t = elem['name']
-        min = 0
-        max = 95
-        print(f" \x1b[36mTITLE: {title_t[min:max]} \x1b[0m")
-        while max < len(title_t):
-            min += 95
-            max += 95
-            print(f" \x1b[36m       {title_t[min:max]} \x1b[0m")
-        print(f" \033[91mDATE: {elem['date']} \x1b[0m")
-        print(f" \x1b[32mDIM: {str(elem['size'])} {elem['type']} \x1b[0m")
-        print(f" \x1b[33mSEED: {elem['seed']} \x1b[0m")
-        print(f" \x1b[37mLEECH: {elem['leech']} \x1b[0m")
-        print(f" \x1b[35mRESOLUTION: {elem['movie_type']} \x1b[0m")
+        min_pos= 0
+        max_pos= 95
+        print(f" {self.cyan}TITLE: {title_t[min_pos:max_pos]} {self.reset_clr}")
+        while max_pos< len(title_t):
+            min_pos+= 95
+            max_pos+= 95
+            print(f" {self.cyan}       {title_t[min_pos:max_pos]} {self.reset_clr}")
+        print(f" {self.red}DATE: {elem['date']} {self.reset_clr}")
+        print(f" {self.green}DIM: {str(elem['size'])} {elem['type']} {self.reset_clr}")
+        print(f" {self.yellow}SEED: {elem['seed']} {self.reset_clr}")
+        print(f" {self.white}LEECH: {elem['leech']} {self.reset_clr}")
+        print(f" {self.magenta}RESOLUTION: {elem['movie_type']} {self.reset_clr}")
 
     def select(self):
         '''Select torrent'''
-        print('\033[4m')
-        print(' '*120)
-        print('\x1b[0m')
+        # write _____________
+        print(f'{self.underscore}'+' '*120+f'{self.reset_clr}\n')
         found = 0
         while found == 0:
             item_dict = json.loads(self.json_torrent)
             try:
                 number = int(input('Choose torrent: '))
             except ValueError:
-                print("")
-                print("\x1b[31;1mNot Valid!!\x1b[0m")
+                print(f"\n{self.red}Not Valid!!{self.reset_clr}\n")
                 self.select()
             number -= 1  # indice di un array
             if number < len(item_dict['Torrent']):
@@ -140,40 +148,32 @@ class TorrentDownloader():
                     for elem in parsed.find_all('a', href=True):
                         if 'magnet' in elem['href']:
                             magnet_link = elem['href']
-                print('\033[4m')
-                print(' '*120)
-                print('\x1b[0m')
                 found = 1
                 self.print_elem(item_dict)
-                conf = input("y to confirm, n to repeat: ")
-                print('\033[4m')
-                print(' '*120)
-                print('\x1b[0m')
-                if conf in ('n', 'N'):
+                conf = ""
+                while conf.lower() not in ['y','n']:
+                    conf = input("\ny to confirm, n to repeat: ")
+                if conf.lower() == 'n':
                     found = 0
-                elif(self.autoadd and (conf in ('y', 'Y'))):
+                elif(self.autoadd and conf.lower() == 'y'):
                     self.cmd_command.append(f"\"{magnet_link}\"")
                     try:
                         subprocess.check_call(self.cmd_command)
                     except (subprocess.CalledProcessError, FileNotFoundError):
-                        print("\x1b[31;1mError, command not found\x1b[0m")
-                        print('\033[4m')
-                        print(' '*120)
-                        print('\x1b[0m')
-                        print(f"Magnet:\x1b[31;1m {magnet_link} \x1b[0m")
+                        print(f"\n{self.red}Error, command not found{self.reset_clr}")
+                        print(f"\nMagnet:{self.red} {magnet_link} {self.reset_clr}\n")
                         sys.exit(0)
-                    print('\x1b[32mSuccess\x1b[0m')
+                    print(f'\n{self.green}Success{self.reset_clr}')
                 else:
-                    print(f"Magnet:\x1b[31;1m {magnet_link} \x1b[0m")
+                    print(f"\nMagnet:{self.red} {magnet_link} {self.reset_clr}\n")
             else:
-                print("")
-                print("\x1b[31;1mOut of range\x1b[0m")
+                print(f"\n{self.red}Out of range{self.reset_clr}")
 
     def __init__(self):
         signal.signal(signal.SIGTERM, self.sig_handler)
         signal.signal(signal.SIGINT, self.sig_handler)
         if len(sys.argv) == 1:
-            name_input = input('Nome Film da cercare:').strip()
+            name_input = input('Nome Film da cercare: ').strip()
         else:
             name_input = sys.argv[1]
             for elem in sys.argv[2:]:
